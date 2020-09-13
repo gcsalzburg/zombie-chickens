b82aa52324be3af742397e51faa1d1b26d4c782e
George Cave <gcbristol@gmail.com>
2020-09-13 17:34:03 +0200

    Remove PHP dependency

--- db.php
+++
@@ -1,9 +0,0 @@
-<?
-function db_connect($db){
-	$host = 'mysql.designedbycave.co.uk'; 
-	$username = 'mysql_zombie'; 
-	$password = 'scea0tait@KEEM'; 
-	$database = $db; 
-	$connection = mysqli_connect($host, $username, $password, $database); 
-}
-?>
